028087f55f6e087e778db7025a098cc25ca80970
MJ-Dev92 <minjae920802@naver.com>
2023-10-23 23:47:00 +0900

    feature/search

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsSearch, BsYoutube } from "react-icons/bs";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SearchHeader() {
-  return <header></header>;
+  const navigate = useNavigate();
+  const [text, setText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`videos/${text}`);
+  };
+  return (
+    <header>
+      <Link to="/">
+        <BsYoutube />
+        <h1>Youtube</h1>
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button>
+          <BsSearch />
+        </button>
+      </form>
+    </header>
+  );
 }
