6481c0899ecee197e47e5be00d741be31bbb839b
MJ-Dev92 <minjae920802@naver.com>
2023-12-26 22:19:14 +0900

    videoCard ui

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React from "react";
 
 export default function VideoCard({ video }) {
-  return <div>{video.snippet.title}</div>;
+  const SP = video.snippet;
+  console.log(SP);
+  const UploadYear =
+    new Date().getFullYear() - new Date(SP.publishedAt).getFullYear();
+  const UploadMonth =
+    new Date().getMonth() - new Date(SP.publishedAt).getMonth();
+  const UploadDay = new Date().getDay() - new Date(SP.publishedAt).getDay();
+  const today = () => {
+    if (UploadYear > 0) {
+      return `${UploadYear} year ago`;
+    } else if (UploadMonth > 0) {
+      return `${UploadMonth} month ago`;
+    } else {
+      return `${UploadDay} day ago`;
+    }
+  };
+  return (
+    <>
+      <img src={SP.thumbnails.default.url} alt="Thumbnails" />
+      <div>{SP.title}</div>
+      <div>{SP.channelTitle}</div>
+      <div>{today()}</div>
+    </>
+  );
 }
