11280fe26614a9b82be4730dd8a2b45392ad3079
MJ-Dev92 <minjae920802@naver.com>
2024-01-09 21:45:38 +0900

    DetailPage featuer

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { formatAgo } from "../util/date";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 export default function VideoCard({ video }) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+
   return (
-    <li>
-      <img className="w-full" src={thumbnails.medium.url} alt="title" />
-      <div>
-        <p className="font-semibold my-2 line-clamp-2">{title}</p>
-        <p className="text-sm opacity-80">{channelTitle}</p>
-        <p className="text-sm opacity-80">{formatAgo(publishedAt, "ko")}</p>
-      </div>
+    <li className="lg:overflow-hidden lg:w-[300px] lg:h-auto mb-2">
+      <Link to={`videos/watch/:${video.id}`}>
+        <img
+          className="w-full lg:hover:scale-110 transition-transform ease-in-out duration-300"
+          src={thumbnails.medium.url}
+          alt="title"
+        />
+        <div>
+          <p className="font-semibold my-2 line-clamp-2">{title}</p>
+          <p className="text-sm opacity-80">{channelTitle}</p>
+          <p className="text-sm opacity-80">{formatAgo(publishedAt, "ko")}</p>
+        </div>
+      </Link>
     </li>
   );
 }
