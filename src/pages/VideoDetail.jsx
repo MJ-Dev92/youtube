80bdd276a72e13f79444e9e7ac1d47adfe8c29f9
MJ-Dev92 <minjae920802@naver.com>
2024-02-19 01:49:42 +0900

    add channelInfo component

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import ChannelInfo from "../components/ChannelInfo";
+import RelatedVideos from "../components/RelatedVideos";
 
 export default function VideoDetail() {
   const {
     state: { video },
   } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
 
-  console.log(video);
-  return <div>VideoDetail</div>;
+  return (
+    <section>
+      <article>
+        <iframe
+          title="video"
+          id="player"
+          type="text/html"
+          width="640"
+          height="390"
+          src={`http://www.youtube.com/embed/${video.id}`}
+        ></iframe>
+        <div>
+          <h2>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle} />
+          <pre>{description}</pre>
+        </div>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
+  );
 }
